2df748527b3c4f9cd3dd4867304f65865631ae52
Rolf <rolfschellenberger@gmail.com>
2021-12-30 16:41:32 +0100

    Moved collection functions to separate file and tests

---
+++ src/main/kotlin/com/rolf/util/Collections.kt
@@ -0,0 +1,56 @@
+package com.rolf.util
+
+
+/**
+ * options: [a, b, c]
+ * return: [[a, b, c], [a, c, b], [b, a, c], [b, c, a], [c, a, b], [c, b, a]]
+ */
+fun <T> getPermutations(options: List<T>, size: Int = options.size, position: Int = 0): List<List<T>> {
+    if (position >= size || options.isEmpty()) {
+        return listOf(emptyList())
+    }
+    if (options.size == 1) {
+        return listOf(listOf(options[0]))
+    }
+
+    val result = mutableListOf<List<T>>()
+    for (option in options) {
+        val newOptions = options.toMutableList()
+        newOptions.remove(option)
+        for (permutation in getPermutations(newOptions, size, position + 1)) {
+            result.add(listOf(option) + permutation)
+        }
+    }
+
+    return result
+}
+
+/**
+ * options: [a, b, c]
+ * return: [[a, b, c], [a, b], [a, c], [a], [b, c], [b], [c]]
+ */
+fun <T> getCombinations(options: List<T>, cache: MutableMap<String, List<List<T>>> = mutableMapOf()): List<List<T>> {
+    if (options.isEmpty()) {
+        return emptyList()
+    }
+    if (options.size == 1) {
+        return listOf(listOf(options.first()))
+    }
+
+    val key = options.joinToString { "-" }
+    if (cache.containsKey(key)) {
+        return cache[key]!!
+    }
+
+    val result = mutableListOf<List<T>>()
+    for (index in 1..options.size) {
+        val current = options[index - 1]
+        val subList = options.subList(index, options.size)
+        for (p in getCombinations(subList, cache)) {
+            result.add(listOf(current) + p)
+        }
+        result.add(listOf(current))
+    }
+    cache[key] = result
+    return result
+}
---
+++ src/test/kotlin/com/rolf/util/CollectionsTest.kt
@@ -0,0 +1,78 @@
+package com.rolf.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CollectionsTest {
+
+    @Test
+    fun testPermutations() {
+        val permutations = getPermutations(listOf("a", "b"))
+        assertEquals(2, permutations.size)
+        assertEquals(listOf(listOf("a", "b"), listOf("b", "a")), permutations)
+
+        val permutations2 = getPermutations(listOf("a", "b", "c"))
+        assertEquals(6, permutations2.size)
+        assertEquals(
+            listOf(
+                listOf("a", "b", "c"),
+                listOf("a", "c", "b"),
+                listOf("b", "a", "c"),
+                listOf("b", "c", "a"),
+                listOf("c", "a", "b"),
+                listOf("c", "b", "a")
+            ), permutations2
+        )
+
+        val permutations3 = getPermutations(listOf("a", "a"))
+        assertEquals(2, permutations3.size)
+        assertEquals(1, permutations3.toSet().size)
+
+        val permutations4 = getPermutations(listOf("a", "a", "b"))
+        assertEquals(6, permutations4.size)
+        assertEquals(3, permutations4.toSet().size)
+
+        assertEquals(5040, getPermutations(listOf("a", "b", "c", "d", "e", "f", "g")).size)
+
+        val permutations5 = getPermutations(listOf("a", "b", "c"), size = 2)
+        assertEquals(6, permutations5.size)
+        assertEquals(
+            listOf(
+                listOf("a", "b"),
+                listOf("a", "c"),
+                listOf("b", "a"),
+                listOf("b", "c"),
+                listOf("c", "a"),
+                listOf("c", "b")
+            ), permutations5
+        )
+    }
+
+    @Test
+    fun testCombinations() {
+        val options = listOf("a", "b", "c", "d")
+        val expected = listOf(
+            listOf("a", "b", "c", "d"),
+            listOf("a", "b", "c"),
+            listOf("a", "b", "d"),
+            listOf("a", "b"),
+            listOf("a", "c", "d"),
+            listOf("a", "c"),
+            listOf("a", "d"),
+            listOf("a"),
+            listOf("b", "c", "d"),
+            listOf("b", "c"),
+            listOf("b", "d"),
+            listOf("b"),
+            listOf("c", "d"),
+            listOf("c"),
+            listOf("d")
+        )
+        val combinations = getCombinations(options)
+        assertEquals(15, combinations.size)
+        assertEquals(expected, getCombinations(options))
+
+        assertEquals(0, getCombinations(listOf<String>()).size)
+        assertEquals(1, getCombinations(listOf("a")).size)
+    }
+}
