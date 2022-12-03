a1d50d53ec6f3d50ee4a127f12bf2b9205b0064f
Rolf <rolfschellenberger@gmail.com>
2022-12-03 06:21:54 +0100

    Day 3

--- src/main/kotlin/com/rolf/day03/Day03.kt
+++ src/main/kotlin/com/rolf/day03/Day03.kt
@@ -8,10 +8,34 @@ fun main() {
 
 class Day03 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        println(
+            lines.asSequence()
+                .map {
+                    it.subSequence(0, it.length / 2).toSet()
+                        .intersect(it.subSequence(it.length / 2, it.length).toSet())
+                        .first()
+                }
+                .map { priority(it) }
+                .sum()
+        )
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        println(
+            lines.chunked(3)
+                .asSequence()
+                .map {
+                    val a = it[0].toSet()
+                    val b = it[1].toSet()
+                    val c = it[2].toSet()
+                    a.intersect(b).intersect(c).first()
+                }
+                .map { priority(it) }
+                .sum()
+        )
+    }
+
+    private fun priority(char: Char): Int {
+        return if (char.code < 97) char.code - 64 + 26 else char.code - 96
     }
 }
