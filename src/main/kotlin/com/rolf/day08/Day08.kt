cc19c83ef27f668edc94a09de6d73542e42df7be
Rolf <rolfschellenberger@gmail.com>
2022-12-08 06:40:55 +0100

    Day 8

--- src/main/kotlin/com/rolf/day08/Day08.kt
+++ src/main/kotlin/com/rolf/day08/Day08.kt
@@ -1,6 +1,7 @@
 package com.rolf.day08
 
 import com.rolf.Day
+import com.rolf.util.Direction
 import com.rolf.util.MatrixInt
 import com.rolf.util.Point
 import com.rolf.util.splitLines
@@ -37,53 +38,19 @@ class Day08 : Day() {
         val point = Point(x, y)
 
         // Move in all directions and when equal or higher value found, it is not visible in that direction
-        val upValue = getUpValues(grid, point).maxOf { it }
-        val downValue = getDownValues(grid, point).maxOf { it }
-        val leftValue = getLeftValues(grid, point).maxOf { it }
-        val rightValue = getRightValues(grid, point).maxOf { it }
-        return upValue < value || downValue < value || leftValue < value || rightValue < value
+        val north = getValues(grid, point, Direction.NORTH).maxOf { it }
+        val east = getValues(grid, point, Direction.EAST).maxOf { it }
+        val south = getValues(grid, point, Direction.SOUTH).maxOf { it }
+        val west = getValues(grid, point, Direction.WEST).maxOf { it }
+        return north < value || east < value || south < value || west < value
     }
 
-    private fun getUpValues(grid: MatrixInt, point: Point): List<Int> {
+    private fun getValues(grid: MatrixInt, point: Point, direction: Direction): List<Int> {
         val result = mutableListOf<Int>()
-        var up = grid.getUp(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result.add(other)
-            up = grid.getUp(up)
-        }
-        return result
-    }
-
-    private fun getDownValues(grid: MatrixInt, point: Point): List<Int> {
-        val result = mutableListOf<Int>()
-        var up = grid.getDown(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result.add(other)
-            up = grid.getDown(up)
-        }
-        return result
-    }
-
-    private fun getLeftValues(grid: MatrixInt, point: Point): List<Int> {
-        val result = mutableListOf<Int>()
-        var up = grid.getLeft(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result.add(other)
-            up = grid.getLeft(up)
-        }
-        return result
-    }
-
-    private fun getRightValues(grid: MatrixInt, point: Point): List<Int> {
-        val result = mutableListOf<Int>()
-        var up = grid.getRight(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result.add(other)
-            up = grid.getRight(up)
+        var next = grid.getForward(point, direction)
+        while (next != null) {
+            result.add(grid.get(next))
+            next = grid.getForward(next, direction)
         }
         return result
     }
@@ -104,65 +71,23 @@ class Day08 : Day() {
         // Move all directions and count the number of trees of height < ours
         val value = grid.get(x, y)
 
-        val upCount = getViewCountUp(grid, Point(x, y), value)
-        val downCount = getViewCountDown(grid, Point(x, y), value)
-        val leftCount = getViewCountLeft(grid, Point(x, y), value)
-        val rightCount = getViewCountRight(grid, Point(x, y), value)
-        return upCount * downCount * leftCount * rightCount
+        val north = getViewCount(grid, Point(x, y), Direction.NORTH, value)
+        val east = getViewCount(grid, Point(x, y), Direction.EAST, value)
+        val south = getViewCount(grid, Point(x, y), Direction.SOUTH, value)
+        val west = getViewCount(grid, Point(x, y), Direction.WEST, value)
+        return north * east * south * west
     }
 
-    private fun getViewCountUp(grid: MatrixInt, point: Point, value: Int): Int {
+    private fun getViewCount(grid: MatrixInt, point: Point, direction: Direction, value: Int): Int {
         var result = 0
-        var up = grid.getUp(point)
-        while (up != null) {
-            val other = grid.get(up)
+        var next = grid.getForward(point, direction)
+        while (next != null) {
+            val other = grid.get(next)
             result++
             if (other >= value) {
                 return result
             }
-            up = grid.getUp(up)
-        }
-        return result
-    }
-
-    private fun getViewCountDown(grid: MatrixInt, point: Point, value: Int): Int {
-        var result = 0
-        var up = grid.getDown(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result++
-            if (other >= value) {
-                return result
-            }
-            up = grid.getDown(up)
-        }
-        return result
-    }
-
-    private fun getViewCountLeft(grid: MatrixInt, point: Point, value: Int): Int {
-        var result = 0
-        var up = grid.getLeft(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result++
-            if (other >= value) {
-                return result
-            }
-            up = grid.getLeft(up)
-        }
-        return result
-    }
-
-    private fun getViewCountRight(grid: MatrixInt, point: Point, value: Int): Int {
-        var result = 0
-        var up = grid.getRight(point)
-        while (up != null) {
-            val other = grid.get(up)
-            result++
-            if (other >= value) {
-                return result
-            }
-            up = grid.getRight(up)
+            next = grid.getForward(next, direction)
         }
         return result
     }
