947ab8e2f370e96b6e7e3993ebbd11afdb6a95cd
Rolf <rolfschellenberger@gmail.com>
2022-12-15 06:40:36 +0100

    Day 15

--- src/main/kotlin/com/rolf/day15/Day15.kt
+++ src/main/kotlin/com/rolf/day15/Day15.kt
@@ -1,6 +1,7 @@
 package com.rolf.day15
 
 import com.rolf.Day
+import com.rolf.util.Point
 
 fun main() {
     Day15().run()
@@ -8,10 +9,67 @@ fun main() {
 
 class Day15 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val sensorsWithBeacons = lines.map { parse(it) }
+        sensorsWithBeacons.forEach { println(it) }
+        val sensorsAndBeacons = sensorsWithBeacons.map {
+            listOf(it.sensor, it.beacon)
+        }.flatten()
+        val beacons = sensorsWithBeacons.map { it.beacon }.toSet()
+
+        val y = 2000000
+        val minX = sensorsAndBeacons.minOf { it.x } * 4
+        val maxX = sensorsAndBeacons.maxOf { it.x } * 4
+        println(minX)
+        println(maxX)
+        var count = 0
+
+        // Calculate distance between sensor and beacon
+        // When inspecting a line, calculate the distance from the point to a sensor
+        // When this distance is lower than the sensor distance, it cannot contain a beacon
+        // When this holds for all sensors, the point cannot contain a beacon
+        for (x in minX..maxX) {
+            val point = Point(x, y)
+            if (beacons.contains(point)) {
+                continue
+            }
+            var canBeBeacon = true
+            for (sensorWithBeacon in sensorsWithBeacons) {
+                val distance = point.distance(sensorWithBeacon.sensor)
+                if (distance <= sensorWithBeacon.distance) {
+                    canBeBeacon = false
+                    break
+                }
+            }
+            if (!canBeBeacon) count++
+        }
+        println(count)
+        //5716881
+    }
+
+    private fun parse(line: String): Sensor {
+        val sensor = parseSensor(line)
+        val beacon = parseBeacon(line)
+        return Sensor(sensor, beacon)
+    }
+
+    private fun parseSensor(line: String): Point {
+        val parts = line.split(": ").first().split(" ")
+        val x = parts[2].split("=")[1].replace(",", "").toInt()
+        val y = parts[3].split("=")[1].toInt()
+        return Point(x, y)
+    }
+
+    private fun parseBeacon(line: String): Point {
+        val parts = line.split(": ")[1].split(" ")
+        val x = parts[4].split("=")[1].replace(",", "").toInt()
+        val y = parts[5].split("=")[1].toInt()
+        return Point(x, y)
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
     }
+}
+
+data class Sensor(val sensor: Point, val beacon: Point) {
+    val distance = sensor.distance(beacon)
 }
