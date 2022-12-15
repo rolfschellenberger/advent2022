ed2504384969a8022788f0264c05af3bfbb27b4d
Rolf Schellenberger <rolf.schellenberger@fastnedcharging.com>
2022-12-15 10:50:59 +0100

    Day 15

--- src/main/kotlin/com/rolf/day15/Day15.kt
+++ src/main/kotlin/com/rolf/day15/Day15.kt
@@ -2,6 +2,8 @@ package com.rolf.day15
 
 import com.rolf.Day
 import com.rolf.util.Point
+import java.util.regex.Pattern
+import kotlin.math.absoluteValue
 
 fun main() {
     Day15().run()
@@ -9,92 +11,91 @@ fun main() {
 
 class Day15 : Day() {
     override fun solve1(lines: List<String>) {
-        if(true) return
         val sensorsWithBeacons = lines.map { parse(it) }
-        sensorsWithBeacons.forEach { println(it) }
-        val sensorsAndBeacons = sensorsWithBeacons.map {
-            listOf(it.sensor, it.beacon)
-        }.flatten()
-        val beacons = sensorsWithBeacons.map { it.beacon }.toSet()
-
         val y = 2000000
-        val minX = sensorsAndBeacons.minOf { it.x } * 4
-        val maxX = sensorsAndBeacons.maxOf { it.x } * 4
-        println(minX)
-        println(maxX)
-        var count = 0
-
-        // Calculate distance between sensor and beacon
-        // When inspecting a line, calculate the distance from the point to a sensor
-        // When this distance is lower than the sensor distance, it cannot contain a beacon
-        // When this holds for all sensors, the point cannot contain a beacon
-        for (x in minX..maxX) {
-            val point = Point(x, y)
-            if (beacons.contains(point)) {
-                continue
-            }
-            var canBeBeacon = true
-            for (sensorWithBeacon in sensorsWithBeacons) {
-                val distance = point.distance(sensorWithBeacon.sensor)
-                if (distance <= sensorWithBeacon.distance) {
-                    canBeBeacon = false
-                    break
-                }
-            }
-            if (!canBeBeacon) count++
-        }
-        println(count)
-        //5716881
-    }
-
-    private fun parse(line: String): Sensor {
-        val sensor = parseSensor(line)
-        val beacon = parseBeacon(line)
-        return Sensor(sensor, beacon)
-    }
-
-    private fun parseSensor(line: String): Point {
-        val parts = line.split(": ").first().split(" ")
-        val x = parts[2].split("=")[1].replace(",", "").toInt()
-        val y = parts[3].split("=")[1].toInt()
-        return Point(x, y)
-    }
-
-    private fun parseBeacon(line: String): Point {
-        val parts = line.split(": ")[1].split(" ")
-        val x = parts[4].split("=")[1].replace(",", "").toInt()
-        val y = parts[5].split("=")[1].toInt()
-        return Point(x, y)
+        val range = getRange(y, sensorsWithBeacons).first()
+        // Remove the existing beacons from the range
+        val beaconsX = sensorsWithBeacons.map { it.beacon }
+            .filter { it.y == y }
+            .map { it.x }
+        val locations = range.toSet() - beaconsX.toSet()
+        println(locations.size)
     }
 
     override fun solve2(lines: List<String>) {
         val sensorsWithBeacons = lines.map { parse(it) }
-        sensorsWithBeacons.forEach { println(it) }
-        val sensorsAndBeacons = sensorsWithBeacons.map {
-            listOf(it.sensor, it.beacon)
-        }.flatten()
-        val beacons = sensorsWithBeacons.map { it.beacon }.toSet()
-
-        // the distress beacon must have x and y coordinates each no lower than 0 and no larger than 4000000.
-        // Build up set of all points in this area
-        // Take every sensor in this area
-        // Remove all points that cannot have a beacon from this sensor cannotHaveBeacon(sensor)
-        // What is left should be the location
-
+        val maxY = 4000000
+        val range = scan(sensorsWithBeacons, 0, maxY)
+        val y = range.first
+        val x = range.second.first().last + 1
+        println(x * maxY.toLong() + y)
     }
 
-//    private fun canContainBeacon(point:Point) {
-//        var canBeBeacon = true
-//        for (sensorWithBeacon in sensorsWithBeacons) {
-//            val distance = point.distance(sensorWithBeacon.sensor)
-//            if (distance <= sensorWithBeacon.distance) {
-//                canBeBeacon = false
-//                break
-//            }
-//        }
-//    }
+    private fun parse(line: String): Sensor {
+        val regex = Pattern.compile("[^\\-0-9]+").toRegex()
+        val clean = line.replace(regex, " ").trim()
+        val (sx, sy, bx, by) = clean.split(" ").map { it.toInt() }
+        return Sensor(Point(sx, sy), Point(bx, by))
+    }
+
+    private fun scan(sensorsWithBeacons: List<Sensor>, yStart: Int, yEnd: Int): Pair<Int, List<IntRange>> {
+        // For each y, see what range is covered by each sensor
+        for (y in yStart..yEnd) {
+            val range = getRange(y, sensorsWithBeacons)
+            // If this range is not 1 range, there must be a location where hidden beacon can be found
+            if (range.size > 1) {
+                return y to range
+            }
+        }
+        throw Exception("No broken range found from $yStart till $yEnd")
+    }
+
+    private fun getRange(y: Int, sensorsWithBeacons: List<Sensor>): List<IntRange> {
+        val intervals = mutableListOf<IntRange>()
+        for (sensorWithBeacon in sensorsWithBeacons) {
+            val distance = sensorWithBeacon.distance
+            val offset = distance - (sensorWithBeacon.sensor.y - y).absoluteValue
+            if (offset < 0) continue
+            val lowX = sensorWithBeacon.sensor.x - offset
+            val highX = sensorWithBeacon.sensor.x + offset
+            intervals.add(lowX..highX)
+        }
+        intervals.sortWith(IntRangeComparator())
+
+        // Merge intervals
+        val range = mutableListOf<IntRange>()
+        for (interval in intervals) {
+            if (range.isEmpty()) {
+                range.add(interval)
+                continue
+            }
+
+            val last = range.last()
+
+            if (interval.first > last.last + 1) {
+                range.add(interval)
+                continue
+            }
+
+            // Extend last range
+            val newInterval = last.first..maxOf(last.last, interval.last)
+            range.removeLast()
+            range.add(newInterval)
+        }
+        return range
+    }
 }
 
 data class Sensor(val sensor: Point, val beacon: Point) {
     val distance = sensor.distance(beacon)
+    override fun toString(): String {
+        return "Sensor(sensor=$sensor, beacon=$beacon, distance=$distance)"
+    }
+}
+
+class IntRangeComparator : Comparator<IntRange> {
+    override fun compare(o1: IntRange, o2: IntRange): Int {
+        return o1.first.compareTo(o2.first)
+    }
+
 }
