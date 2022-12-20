3b83c3d2cc7bba6e37ef76a365a311e93dfe8a27
Rolf <rolfschellenberger@gmail.com>
2022-12-20 09:04:44 +0100

    Day 20

--- src/main/kotlin/com/rolf/day20/Day20.kt
+++ src/main/kotlin/com/rolf/day20/Day20.kt
@@ -8,10 +8,81 @@ fun main() {
 
 class Day20 : Day() {
     override fun solve1(lines: List<String>) {
-        TODO("Not yet implemented")
+        val list = buildLinkedList(lines)
+        mix(list, 1)
+        println(find(list, 1000) + find(list, 2000) + find(list, 3000))
     }
 
     override fun solve2(lines: List<String>) {
-        TODO("Not yet implemented")
+        val list = buildLinkedList(lines, 811589153)
+        mix(list, 10)
+        println(find(list, 1000) + find(list, 2000) + find(list, 3000))
+    }
+
+    private fun buildLinkedList(lines: List<String>, decryptionKey: Long = 1): List<Element> {
+        val list = lines
+            .map { it.toLong() }
+            .map { it * decryptionKey }
+            .map { Element(it) }
+        for ((index, element) in list.withIndex()) {
+            val previousIndex = (index - 1 + list.size) % list.size
+            val nextIndex = (index + 1) % list.size
+            element.previous = list[previousIndex]
+            element.next = list[nextIndex]
+        }
+        return list
+    }
+
+    private fun mix(list: List<Element>, count: Int) {
+        for (i in 0 until count) {
+            list.forEach { move(it, list.size) }
+        }
+    }
+
+    private fun move(element: Element, listSize: Int) {
+        if (element.value == 0L) return
+
+        // Remove from list
+        element.previous?.next = element.next
+        element.next?.previous = element.previous
+
+        // Move element.value to the right
+        var pointer = element.next!!
+        // We are moving listSize - 1 locations to do a full round, so modulo = listSize - 1
+        val modulo = listSize - 1
+        val moveRight = ((element.value % modulo) + modulo) % modulo
+        for (i in 0 until moveRight) {
+            pointer = pointer.next!!
+        }
+
+        // Put back in place
+        element.next = pointer
+        element.previous = pointer.previous
+        element.next?.previous = element
+        element.previous?.next = element
+    }
+
+    private fun find(list: List<Element>, shift: Int): Long {
+        val zero = findZero(list)
+        var pointer = zero
+        for (i in 0 until shift % list.size) {
+            pointer = pointer.next!!
+        }
+        return pointer.value
+    }
+
+    private fun findZero(list: List<Element>): Element {
+        for (element in list) {
+            if (element.value == 0L) return element
+        }
+        throw Exception("Zero not found!")
+    }
+}
+
+data class Element(val value: Long) {
+    var previous: Element? = null
+    var next: Element? = null
+    override fun toString(): String {
+        return "Element(value=$value, previous=${previous?.value}, next=${next?.value})"
     }
 }
