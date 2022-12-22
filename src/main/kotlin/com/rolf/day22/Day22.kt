7bebd134be1b758246f11ed58bae6323a30feca7
Rolf Schellenberger <rolf.schellenberger@fastnedcharging.com>
2022-12-22 14:59:59 +0100

    Day 22

--- src/main/kotlin/com/rolf/day22/Day22.kt
+++ src/main/kotlin/com/rolf/day22/Day22.kt
@@ -12,16 +12,10 @@ class Day22 : Day() {
         val (maze, p) = groupLines(lines, "")
         val path = p.first()
         val grid = buildGrid(maze)
-//        println(path)
-//        println(grid)
-
         val instructions = parseInstructions(path)
-//        println(instructions)
-
         var location = grid.find(".").first()
-//        println(location)
-
         var direction = Direction.EAST
+
         for (instruction in instructions) {
             if (instruction.isNumeric()) {
                 val steps = instruction.toInt()
@@ -37,19 +31,14 @@ class Day22 : Day() {
                     }
                     location = newLocation
                 }
-//                println("$steps -> $location")
-//                printGrid(grid, location)
             } else {
-//                println(instruction)
                 direction = when (instruction) {
                     "R" -> direction.right()
                     "L" -> direction.left()
                     else -> throw Exception("Unknown direction: $instruction")
                 }
-//                println(direction)
             }
         }
-//        println(location)
         val facing = when (direction) {
             Direction.EAST -> 0
             Direction.SOUTH -> 1
@@ -68,7 +57,7 @@ class Day22 : Day() {
         for (y in maze.indices) {
             val line = maze.get(y)
             for (x in line.indices) {
-                var char = line.get(x)
+                val char = line.get(x)
                 grid.set(x, y, char.toString())
             }
         }
@@ -101,70 +90,167 @@ class Day22 : Day() {
     }
 
     override fun solve2(lines: List<String>) {
-        // What options do we have?
-
-        // Find out when you walk off the map or into the void, how to translate your point
-        // to the right location. That's hard, because you don't know if you should wrap or transform.
-
-        // Split map into 6 smaller maps
-        // Know what is your neighbour on 4 sides
-        // When walking off the grid, continue to the next on the right location.
-        // This will require rotating the location somehow?
-
-        // Go manual: draw until a movement leads to a void
-        // Manually determine the new location
-        // This is actually mapping the movement locations
         val (maze, p) = groupLines(lines, "")
         val path = p.first()
         val grid = buildGrid(maze)
-//        println(path)
-//        println(grid)
-
         val instructions = parseInstructions(path)
-//        println(instructions)
+        var location = Location(grid.find(".").first(), Direction.EAST)
+        val paths = mutableListOf<List<Location>>()
 
-        var location = grid.find(".").first()
-//        println(location)
-
-        var direction = Direction.EAST
         for (instruction in instructions) {
             if (instruction.isNumeric()) {
+                val locations = mutableListOf<Location>()
+//                println("$instruction to ${location.direction}")
                 val steps = instruction.toInt()
-
                 for (i in 0 until steps) {
-                    var newLocation = grid.getForward(location, direction)
-                    if (newLocation == null || grid.get(newLocation) == " ") {
-                        println("Void: $direction: $location")
-                        printGrid(grid, location)
-                        return
-                    }
-                    // Cannot move
-                    else if (grid.get(newLocation) != "#") {
-                        location = newLocation
-                    }
+                    location = move(grid, location)
+                    locations.add(location)
                 }
-//                println("$steps -> $location")
-//                printGrid(grid, location)
+                paths.add(locations)
             } else {
-//                println(instruction)
-                direction = when (instruction) {
-                    "R" -> direction.right()
-                    "L" -> direction.left()
+                val newDirection = when (instruction) {
+                    "R" -> location.direction.right()
+                    "L" -> location.direction.left()
                     else -> throw Exception("Unknown direction: $instruction")
                 }
-//                println(direction)
+                location = location.copy(direction = newDirection)
+                println("${location.point.x},${location.point.y}")
+                if (location.point.x == 81 && location.point.y == 0) {
+                    printGrid(grid, paths)
+                }
             }
         }
+
+//        printGrid(grid, paths)
+
 //        println(location)
-        val facing = when (direction) {
+        val facing = when (location.direction) {
             Direction.EAST -> 0
             Direction.SOUTH -> 1
             Direction.WEST -> 2
             Direction.NORTH -> 3
         }
-        println(1000 * (location.y + 1) + 4 * (location.x + 1) + facing)
+        println(1000 * (location.point.y + 1) + 4 * (location.point.x + 1) + facing)
+        // 112032 too low
+    }
 
+    private fun printGrid(grid: MatrixString, paths: List<List<Location>>) {
+        val copy = grid.copy()
+        var value = 0
+        for (locations in paths) {
+            for (location in locations) {
+                copy.set(location.point, value.toString())
+            }
+            value = (value + 1) % 10
+        }
+        println(copy)
+    }
+
+    private fun move(grid: MatrixString, location: Location): Location {
+        val x = location.point.x
+        val y = location.point.y
+        val direction = location.direction
+        var newLocation: Location? = null
+
+        // Map edges to edges and direction to a new direction via its transformation
+        // Blocks are 50 x 50
+        // Going from left to right, top to bottom for each block and open edge
+        if (direction == Direction.NORTH && x in 50 until 100 && y == 0) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(0, x + 100))
+            newLocation = Location(Point(0, x + 100), Direction.EAST)
+        }
+        if (direction == Direction.NORTH && x in 100 until 150 && y == 0) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(x - 100, 199))
+            newLocation = Location(Point(x - 100, 199), Direction.NORTH)
+        }
+        if (direction == Direction.WEST && x == 50 && y in 0 until 50) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(0, 49 - y + 100))
+            newLocation = Location(Point(0, 49 - y + 100), Direction.EAST)
+        }
+        if (direction == Direction.EAST && x == 149 && y in 0 until 50) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(99, 49 - y + 100))
+            newLocation = Location(Point(99, 49 - y + 100), Direction.WEST)
+        }
+        // Next row starting with top edges
+        if (direction == Direction.SOUTH && x in 100 until 150 && y == 49) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(99, x - 50))
+            newLocation = Location(Point(99, x - 50), Direction.WEST)
+        }
+        if (direction == Direction.WEST && x == 50 && y in 50 until 100) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(y - 50, 100))
+            newLocation = Location(Point(y - 50, 100), Direction.SOUTH)
+        }
+        if (direction == Direction.EAST && x == 99 && y in 50 until 100) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(y + 50, 49))
+            newLocation = Location(Point(y + 50, 49), Direction.NORTH)
+        }
+        // last 3 blocks, starting top edges
+        if (direction == Direction.NORTH && x in 0 until 50 && y == 100) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(50, x + 50))
+            newLocation = Location(Point(50, x + 50), Direction.EAST)
+        }
+        if (direction == Direction.WEST && x == 0 && y in 100 until 150) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(50, (49 - (y - 100)) + 100 - 100))
+            newLocation = Location(Point(50, (49 - (y - 100)) + 100 - 100), Direction.EAST)
+        }
+        if (direction == Direction.EAST && x == 99 && y in 100 until 150) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(149, (49 - (y - 100)) + 100 - 100))
+            newLocation = Location(Point(149, (49 - (y - 100)) + 100 - 100), Direction.WEST)
+        }
+        // last block, starting top edges
+        if (direction == Direction.SOUTH && x in 50 until 100 && y == 149) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(49, x + 100))
+            newLocation = Location(Point(49, x + 100), Direction.WEST)
+        }
+        if (direction == Direction.WEST && x == 0 && y in 150 until 200) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(y - 100, 0))
+            newLocation = Location(Point(y - 100, 0), Direction.WEST)
+        }
+        if (direction == Direction.EAST && x == 49 && y in 150 until 200) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(y - 100, 149))
+            newLocation = Location(Point(y - 100, 149), Direction.NORTH)
+        }
+        if (direction == Direction.SOUTH && x in 0 until 50 && y == 199) {
+//            printGrid(grid, location.point)
+//            printGrid(grid, Point(x + 100, 0))
+            newLocation = Location(Point(x + 100, 0), Direction.SOUTH)
+        }
+
+        // Can we move across edges?
+        if (newLocation != null) {
+            if (grid.get(newLocation.point) == "#") {
+//                printGrid(grid, location.point)
+//                printGrid(grid, newLocation.point)
+                return location
+            }
+            return newLocation
+        }
+
+        // Move forward on the map
+        val forwardLocation = grid.getForward(location.point, location.direction)
+        if (forwardLocation == null) {
+            printGrid(grid, location.point)
+            return location
+        }
+        // Cannot move through walls
+        if (grid.get(forwardLocation) == "#") {
+            return location
+        }
+        return location.copy(point = forwardLocation)
     }
 }
 
-data class Map<T, U>(val grid: MatrixString, val connections: Map<Direction, MatrixString>)
+data class Location(val point: Point, val direction: Direction)
