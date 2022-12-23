ead7de4298b79e06308f25b963b206db01bc04c2
Rolf Schellenberger <rolf.schellenberger@fastnedcharging.com>
2022-12-23 12:04:39 +0100

    Day 23

--- src/main/kotlin/com/rolf/day23/Day23.kt
+++ src/main/kotlin/com/rolf/day23/Day23.kt
@@ -9,101 +9,61 @@ fun main() {
 
 class Day23 : Day() {
     override fun solve1(lines: List<String>) {
-        val grid = MatrixString.build(splitLines(lines))
-//        println(grid)
-        // 0=N 1=S 2=W 3=E
-        val moves = charArrayOf('N', 'S', 'W', 'E')
-        var rounds = 0
-//        for (i in 0 until 10) {
-        while (true) {
-            rounds++
-//            println()
-//            println(grid)
-            if (grid.getLeftEdge().contains("#") ||
-                grid.getTopEdge().contains("#") ||
-                grid.getRightEdge().contains("#") ||
-                grid.getBottomEdge().contains("#")
-            ) {
-                grid.grow(1, 1, 1, 1, ".")
-            }
-//            println(moves)
-
-            val movesMap = mutableMapOf<Point, Pair<Point, Int>>()
-            val elves = grid.find("#").toSet()
-            for (elf in elves) {
-                val newPos = newPosition(grid, elves, elf, moves)
-                if (newPos != elf) {
-                    movesMap.computeIfAbsent(newPos) { elf to 0 }
-                    movesMap.computeIfPresent(newPos) { _, v -> v.first to v.second + 1 }
-//                println("$elf -> $newPos")
-                }
-            }
-
-            // Inspect the new positions that have only 1 elf move to it
-            val movingElves = movesMap.filter { (_, value) -> value.second == 1 }
-            println("${movingElves.size} elves move")
-            if (movingElves.isEmpty()) {
-                break
-            }
-            for (movingElf in movingElves) {
-                val from = movingElf.value.first
-                val to = movingElf.key
-//                println("$from -> $to")
-                grid.set(from, ".")
-                grid.set(to, "#")
-            }
-
-            moves.pushLeft(1)
-        }
-//        println(grid)
+        val elves = moveElves(lines, 10).first
 
         // Now take the smallest rectangle
-        val elves = grid.find("#")
         val minX = elves.minOf { it.x }
         val maxX = elves.maxOf { it.x }
         val minY = elves.minOf { it.y }
         val maxY = elves.maxOf { it.y }
-        grid.cutOut(Point(minX, minY), Point(maxX, maxY))
-        println(grid)
-        println(grid.count("."))
-        println(rounds)
+        val width = maxX - minX + 1
+        val height = maxY - minY + 1
+        println((width * height) - elves.size)
     }
 
-    private fun newPosition(grid: MatrixString, elves: Set<Point>, elf: Point, moves: CharArray): Point {
-        val neighbours = grid.getNeighbours(elf)
-        if (elves.intersect(neighbours).isEmpty()) {
-            // Do nothing
-//            println("No elfs around me!")
-            return elf
-        }
-        for (move in moves) {
-            val direction = getDirection(move)
-            // When there are no elves in the moving direction
-            if (canMove(grid, elves, elf, direction)) {
-                // Move into that direction
-//                println("$elf: $direction")
-                return grid.getForward(elf, direction)!!
-            }
-        }
-        // Stay put
-        return elf
+    override fun solve2(lines: List<String>) {
+        println(moveElves(lines, Int.MAX_VALUE).second)
     }
 
-    private fun canMove(grid: MatrixString, elves: Set<Point>, elf: Point, direction: Direction): Boolean {
-        val adjacent = when (direction) {
-            Direction.NORTH -> listOf(grid.getUp(elf), grid.getLeftUp(elf), grid.getRightUp(elf))
-            Direction.SOUTH -> listOf(grid.getDown(elf), grid.getLeftDown(elf), grid.getRightDown(elf))
-            Direction.WEST -> listOf(grid.getLeft(elf), grid.getLeftUp(elf), grid.getLeftDown(elf))
-            Direction.EAST -> listOf(grid.getRight(elf), grid.getRightUp(elf), grid.getRightDown(elf))
-        }
+    private fun moveElves(lines: List<String>, maxRounds: Int): Pair<Set<Point>, Int> {
+        val grid = MatrixString.build(splitLines(lines))
+        val moves = charArrayOf('N', 'S', 'W', 'E')
+        val elves = grid.find("#").toMutableSet()
+        var rounds = 0
+        for (i in 0 until maxRounds) {
+            rounds++
 
-        // We can move if there is no elf in the adjacent positions
-        for (point in adjacent) {
-            if (elves.contains(point)) {
-                return false
+            val directions = moves.map { getDirection(it) }
+            val movements = elves.map {
+                it to newPosition(elves, it, directions)
             }
+
+            // When all the intended movements are known, get the ones that end up on unique locations
+            val possibleMovements = movements
+                // Keep actual movements
+                .filter { (k, v) -> k != v }
+                // Group by target locations
+                .groupBy { (_, v) -> v }
+                // Only keep the ones where only 1 elf moves to
+                .filter { (_, v) -> v.size == 1 }
+                // Get back the original movements (from, to)
+                .map { (_, v) -> v.first() }
+
+            // When there are no elves moving, we can stop
+            if (possibleMovements.isEmpty()) {
+                break
+            }
+
+            // Since every elf is on a unique location, remove the old locations and add the new
+            for (p in possibleMovements) {
+                elves.remove(p.first)
+                elves.add(p.second)
+            }
+
+            // Rotate the moves
+            moves.pushLeft(1)
         }
-        return true
+        return elves to rounds
     }
 
     private fun getDirection(move: Char): Direction {
@@ -116,6 +76,57 @@ class Day23 : Day() {
         }
     }
 
-    override fun solve2(lines: List<String>) {
+    private fun newPosition(elves: Set<Point>, elf: Point, moves: List<Direction>): Point {
+        val neighbours = getNeighbours(elf)
+        // When there are no elves around the elf, don't move.
+        if (elves.intersect(neighbours).isEmpty()) {
+            return elf
+        }
+
+        // Otherwise try to move in the order of the moves
+        for (move in moves) {
+            // When there are no elves in the moving direction
+            if (canMove(elves, elf, move)) {
+                // Move into that direction
+                return move(elf, move)
+            }
+        }
+
+        // Stay put
+        return elf
+    }
+
+    private fun getNeighbours(elf: Point): Set<Point> {
+        return setOf(
+            Point(elf.x, elf.y - 1),
+            Point(elf.x, elf.y + 1),
+            Point(elf.x + 1, elf.y - 1),
+            Point(elf.x + 1, elf.y),
+            Point(elf.x + 1, elf.y + 1),
+            Point(elf.x - 1, elf.y - 1),
+            Point(elf.x - 1, elf.y),
+            Point(elf.x - 1, elf.y + 1)
+        )
+    }
+
+    private fun canMove(elves: Set<Point>, elf: Point, direction: Direction): Boolean {
+        val adjacent = when (direction) {
+            Direction.NORTH -> setOf(Point(elf.x, elf.y - 1), Point(elf.x - 1, elf.y - 1), Point(elf.x + 1, elf.y - 1))
+            Direction.SOUTH -> setOf(Point(elf.x, elf.y + 1), Point(elf.x - 1, elf.y + 1), Point(elf.x + 1, elf.y + 1))
+            Direction.WEST -> setOf(Point(elf.x - 1, elf.y), Point(elf.x - 1, elf.y - 1), Point(elf.x - 1, elf.y + 1))
+            Direction.EAST -> setOf(Point(elf.x + 1, elf.y), Point(elf.x + 1, elf.y - 1), Point(elf.x + 1, elf.y + 1))
+        }
+
+        // We can move if there is no elf in the adjacent positions
+        return adjacent.all { !elves.contains(it) }
+    }
+
+    private fun move(elf: Point, direction: Direction): Point {
+        return when (direction) {
+            Direction.NORTH -> Point(elf.x, elf.y - 1)
+            Direction.SOUTH -> Point(elf.x, elf.y + 1)
+            Direction.WEST -> Point(elf.x - 1, elf.y)
+            Direction.EAST -> Point(elf.x + 1, elf.y)
+        }
     }
 }
